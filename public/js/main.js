b916c0455bfa14d8ed15e45417636d6a9fc41651
dsawali <dsawali@sfu.ca>
2018-10-28 13:21:54 -0700

    Added function to get input box value

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,16 @@
 let searchContext ='hashtag';
+let inputTextValue ='';
 
+// Stores value of dropdown menu in a variable
 function selectDropdownValue() {
     searchContext = document.getElementById('search-context').value;
     console.log(searchContext);
 }
 
+// Stores value of input box in a variable
+function getInputTextValue() {
+    inputTextValue = document.getElementById('search-box').value;
+    console.log(inputTextValue);
+}
 
 
